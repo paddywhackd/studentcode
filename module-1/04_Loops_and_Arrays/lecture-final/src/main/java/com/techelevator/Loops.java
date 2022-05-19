69f65bbc50f449bcde7207d2d4e623142a705fad
mike levy <michael.levy@techelevator.com>
2022-05-19 12:51:13 -0400

    M1D4 lecture final

---
+++ module-1/04_Loops_and_Arrays/lecture-final/src/main/java/com/techelevator/Loops.java
@@ -0,0 +1,33 @@
+package com.techelevator;
+
+public class Loops {
+    public static void main(String[] args) {
+
+
+
+
+//        for (int counter = 1; counter < 100; counter*=2 )  {
+//            System.out.println(counter);
+//        }
+
+//        for (int counter = 100; counter > 0; counter-- ) {
+//            System.out.println(counter);
+//        }
+
+
+//        int counter = 100;
+//        while (counter > 0) {
+//            System.out.println(counter);
+//
+//            if (counter == 4) {
+////                break;
+//                continue;
+//            }
+//            counter--;
+//
+//            System.out.println("hello");
+//        }
+
+
+    }
+}
