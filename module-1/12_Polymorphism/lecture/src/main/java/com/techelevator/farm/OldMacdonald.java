1fb23798f6fa303330f4183bbde6d14c9b4282a7
mike levy <michael.levy@techelevator.com>
2022-06-01 08:49:13 -0400

    M1D12 - lecture & exercises

---
+++ module-1/12_Polymorphism/lecture/src/main/java/com/techelevator/farm/OldMacdonald.java
@@ -0,0 +1,17 @@
+package com.techelevator.farm;
+
+public class OldMacdonald {
+	public static void main(String[] args) {
+
+
+	}
+
+	public static void playVerse(String name, String sound) {
+			System.out.println("Old MacDonald had a farm, ee, ay, ee, ay, oh!");
+			System.out.println("And on his farm he had a " + name + ", ee, ay, ee, ay, oh!");
+			System.out.println("With a " + sound + " " + sound + " here");
+			System.out.println("And a " + sound + " " + sound + " there");
+			System.out.println("Here a " + sound + " there a " + sound + " everywhere a " + sound + " " + sound);
+			System.out.println();
+	}
+}
