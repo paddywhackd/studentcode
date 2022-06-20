f463b9dc0cce661903b7a1be851402380031991d
Patrick Morris <pnmorris513@gmail.com>
2022-06-20 07:40:29 -0400

    insert, delete exercise

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/09_remove_movie.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/09_remove_movie.sql
@@ -1,3 +1,6 @@
 -- 9. Remove "Memento" from the movie table
 -- You'll have to remove data from two other tables before you can remove it (13 rows, 2 rows, 1 row)
 
+DELETE FROM movie_actor WHERE movie_id = (select movie_id FROM movie where title = 'Memento');
+DELETE FROM movie_genre WHERE movie_id = (SELECT movie_id from movie where title = 'Memento');
+DELETE FROM movie WHERE title = 'Memento';
