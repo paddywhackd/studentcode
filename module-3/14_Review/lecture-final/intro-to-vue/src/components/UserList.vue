10517faf2903ca3bb0b4f2e41db6982e453ec7f1
mike levy <michael.levy@techelevator.com>
2022-07-29 12:46:24 -0400

    M3D14 - final lecture files

---
+++ module-3/14_Review/lecture-final/intro-to-vue/src/components/UserList.vue
@@ -0,0 +1,140 @@
+<template>
+  <table id="tblUsers">
+    <thead>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Username</th>
+        <th>Email Address</th>
+        <th>Status</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <input type="text" id="firstNameFilter" v-model="filter.firstName" />
+        </td>
+        <td>
+          <input type="text" id="lastNameFilter" v-model="filter.lastName" />
+        </td>
+        <td>
+          <input type="text" id="usernameFilter" v-model="filter.username" />
+        </td>
+        <td>
+          <input type="text" id="emailFilter" v-model="filter.emailAddress" />
+        </td>
+        <td>
+          <select id="statusFilter" v-model="filter.status">
+            <option value="">Show All</option>
+            <option value="Active">Active</option>
+            <option value="Disabled">Disabled</option>
+          </select>
+        </td>
+      </tr>
+      <!-- user listing goes here -->
+      <tr v-for="user in filteredList" :key="user.username">
+        <td>{{ user.firstName }}</td>
+        <td>{{ user.lastName }}</td>
+        <td>{{ user.username }}</td>
+        <td>{{ user.emailAddress }}</td>
+        <td>{{ user.status }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+function insensitiveContains(str1, str2) {
+  return !str2 || str1.toUpperCase().includes(str2.toUpperCase());
+}
+
+export default {
+  name: "user-list",
+  data() {
+    return {
+      users: [
+        {
+          firstName: "John",
+          lastName: "Smith",
+          username: "jsmith",
+          emailAddress: "jsmith@gmail.com",
+          status: "Active",
+        },
+        {
+          firstName: "Anna",
+          lastName: "Bell",
+          username: "abell",
+          emailAddress: "abell@yahoo.com",
+          status: "Active",
+        },
+        {
+          firstName: "George",
+          lastName: "Best",
+          username: "gbest",
+          emailAddress: "gbest@gmail.com",
+          status: "Disabled",
+        },
+        {
+          firstName: "Ben",
+          lastName: "Carter",
+          username: "bcarter",
+          emailAddress: "bcarter@gmail.com",
+          status: "Active",
+        },
+        {
+          firstName: "Katie",
+          lastName: "Jackson",
+          username: "kjackson",
+          emailAddress: "kjackson@yahoo.com",
+          status: "Active",
+        },
+        {
+          firstName: "Mark",
+          lastName: "Smith",
+          username: "msmith",
+          emailAddress: "msmith@foo.com",
+          status: "Disabled",
+        },
+      ],
+      filter: {
+        firstName: "",
+        lastName: "",
+        username: "",
+        emailAddress: "",
+        status: "",
+      },
+    };
+  },
+  computed: {
+    filteredList() {
+      return this.users.filter((u) => {
+        return (
+          insensitiveContains(u.firstName, this.filter.firstName) &&
+          insensitiveContains(u.lastName, this.filter.lastName)
+        );
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+table {
+  margin-top: 20px;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+th {
+  text-transform: uppercase;
+}
+td {
+  padding: 10px;
+}
+tr.disabled {
+  color: red;
+}
+input,
+select {
+  font-size: 16px;
+}
+</style>
---
+++ module-3/14_Review/lecture-final/review-page/src/components/ReviewTableRow.vue
@@ -0,0 +1,52 @@
+<template>
+  <tr>
+    <td>{{ review.title }}</td>
+    <td nowrap>{{ review.reviewer }}</td>
+    <td>{{ review.review }}</td>
+    <td class="stars">
+      <img
+        src="../assets/star.png"
+        v-bind:title="review.rating + ' Star Review'"
+        class="star"
+        v-for="n in review.rating"
+        v-bind:key="n"
+        width="20px"
+      />
+    </td>
+    <td>
+      <input
+        type="checkbox"
+        v-bind:checked="review.done"
+        v-on:change="onFavoritedChange(review)"
+      />
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "ReviewTableRow",
+  props: ["review"],
+  methods: {
+    onFavoritedChange() {
+      this.$store.commit("FLIP_FAVORITED", this.review);
+    },
+  },
+};
+</script>
+
+<style scoped>
+td {
+  text-align: left;
+}
+td {
+  padding-right: 10px;
+  vertical-align: top;
+}
+tr:nth-child(even) {
+  background-color: rgb(238, 238, 238);
+}
+.stars {
+  display: flex;
+}
+</style>
